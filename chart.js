a5a10d373427e99ce51a9fc6d4f713a69ca15b66
Nikikpt <36451853+Nikikpt@users.noreply.github.com>
2018-08-15 19:32:11 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -94,10 +94,7 @@ function start() {
 		.attr("r", 0)
 		.style("fill", function(d) { return fill(d.party); })
 		.on("mouseover", mouseover)
-		.on("mouseout", mouseout);
-	        .on("click", function(d) {
-	        window.open("https://www.google.com/search?q=" +  d.donor);
-		});
+		.on("mouseout", mouseout)
 		// Alternative title based 'tooltips'
 		// node.append("title")
 		//	.text(function(d) { return d.donor; });
