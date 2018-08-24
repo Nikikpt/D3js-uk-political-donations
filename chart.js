ae3a62de8120a825377d9bc6fb0d7dddba629ae6
Nikikpt <36451853+Nikikpt@users.noreply.github.com>
2018-08-24 18:50:45 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -275,30 +275,6 @@ function moveToFunds(alpha) {
 	};
 	
 	
-	function moveToDonationAmounts(alpha) {
-	return function(d) {
-		var centreX = svgCentre.x + 75;
-			if (d.value <= 25001) {
-				centreY = svgCentre.y + 75;
-			} else if (d.value <= 50001) {
-				centreY = svgCentre.y + 55;
-			} else if (d.value <= 100001) {
-				centreY = svgCentre.y + 35;
-			} else  if (d.value <= 500001) {
-				centreY = svgCentre.y + 15;
-			} else  if (d.value <= 1000001) {
-				centreY = svgCentre.y - 5;
-			} else  if (d.value <= maxVal) {
-				centreY = svgCentre.y - 25;
-			} else {
-				centreY = svgCentre.y;
-			}
-
-		d.x += (centreX - d.x) * (brake + 0.06) * alpha * 1.2;
-		d.y += (centreY - 100 - d.y) * (brake + 0.06) * alpha * 1.2;
-	};
-
-
 }
 
 // Collision detection function by m bostock
