c81cbbd6f56833001cfe6160be41124655e52655
Nikikpt <36451853+Nikikpt@users.noreply.github.com>
2018-08-29 00:40:23 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -407,23 +407,10 @@ function mouseover(d, i) {
 	
 
 	
-	var http = new XMLHttpRequest();
-	http.open('HEAD', imageFile, false);
-	http.send();
-	if (http.status != 404){
-		if ((names.indexOf(donor) > -1)==false) {
-		
-		var elem = document.createElement("img");
-                //img.id = idCount;
-                //idCount = idCount+1;	
-		elem.src = imageFile;
-		elem.setAttribute("height", "42");
-		elem.setAttribute("width", "42");
-
-		document.getElementById("sidebar").appendChild(elem);
-		names.push(donor);	
-		}
-	}		
+		var newIcon = $("#IconContainer").html();
+var addIt = donor+"<div><img src='" + imageFile +"' class='icon-image' align='middle' onError='this.src=\"https://github.com/favicon.ico\";'/>"+
+	"</div>"
+	$("#IconContainer").html(addIt + newIcon);
 	
 }
 	
